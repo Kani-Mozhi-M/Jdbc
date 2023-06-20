1651ab4be5182d27a7d4794c19fe7057107f427b
M Kanimozhi <133675777+Kani-Mozhi-M@users.noreply.github.com>
2023-06-20 13:15:31 +0530

    Add files via upload

---
+++ ReadRecords.java
@@ -0,0 +1,20 @@
+import java.sql.*;
+
+public class ReadRecords {
+    public static void main(String[] args) {
+        try {
+            //reading the records
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Lk", "root", " ");
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery("select * from employee2");
+
+            while (rs.next()) {
+                System.out.println(rs.getInt(1));
+                System.out.println(rs.getString(2));
+                System.out.println(rs.getString(3));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
